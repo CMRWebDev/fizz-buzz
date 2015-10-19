c59bfb491d9035648ddc12fe81731c8426296fee
CMRWebDev <crosenberg690@gmail.com>
2015-10-18 21:18:02 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function() {
+
+for (i = 1; i < 101; i++) {
+	if ((i % 3 == 0 && i % 5 == 0)) {
+		document.write("FizzBuzz");
+		document.write("<br>");
+	}
+	else if (i % 3 == 0) {
+		document.write("Fizz");
+		document.write("<br>");
+	}
+	else if (i % 5 == 0) {
+		document.write("Buzz");
+		document.write("<br>");
+	}
+	else {
+		document.write(i);
+		document.write("<br>");
+	}
+}
+});
+
+
