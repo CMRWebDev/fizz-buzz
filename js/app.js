22f5e1549d4cf40b73dbb2daa17feda1db719e9b
CMRWebDev <crosenberg690@gmail.com>
2015-10-20 18:09:35 -0400

    Refactor-Complete

--- js/app.js
+++ js/app.js
@@ -1,36 +1,51 @@
 
 $(document).ready(function() {
 
- 
+ $("#button-submit").click(function(){
+ 	
+
+ 	// Variables
+ 	var user_choice = document.getElementById('user_number');
+ 	var user_choice_val = user_choice.value;
+ 	var user_choice_num = Number(user_choice_val);
+ 	
+
+ 	
+	function FzBzFunction(max){
+
+	for (i = 1; i <= max; i++) {
+		if ((i % 3 == 0 && i % 5 == 0)) {
+			document.write("FizzBuzz");
+			document.write("<br>");
+		}
+		else if (i % 3 == 0) {
+			document.write("Fizz");
+			document.write("<br>");
+		}
+		else if (i % 5 == 0) {
+			document.write("Buzz");
+			document.write("<br>");
+		}
+		else {
+			document.write(i);
+			document.write("<br>");
+		}
+	}
+}
+
+ FzBzFunction(user_choice_num);
+
+});
+
+});
+
 // convert string to number
 
 // validate that number is a int not a float 
-var number = Number(user-number);
-user-number % 1 != 0
 
 
-function FzBzFunction(user-number){
-
-for (i = 1; i < 101; i++) {
-	if ((i % 3 == 0 && i % 5 == 0)) {
-		document.write("FizzBuzz");
-		document.write("<br>");
-	}
-	else if (i % 3 == 0) {
-		document.write("Fizz");
-		document.write("<br>");
-	}
-	else if (i % 5 == 0) {
-		document.write("Buzz");
-		document.write("<br>");
-	}
-	else {
-		document.write(i);
-		document.write("<br>");
-	}
-}
-}
-});
 
 
-isNaN(num) 
+
+
+
